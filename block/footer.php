d2fa9cc378ce5066d9a23748f4634376cfbe93c8
mELODIA <90747698+E4ron@users.noreply.github.com>
2022-06-12 15:25:42 +0500

    Add files via upload

---
+++ block/footer.php
@@ -0,0 +1,12 @@
+<footer>
+<div class="footer-container footer">
+     <div class="footer-content">
+            <p class="footer-number">8(35235)41147</p>
+            <p class="footer-phone">Телефон</p>
+        </div>
+        <div>
+            <p class="footer-number">aktaban1@mail.ru</p>
+            <p class="footer-phone">E-mail</p>
+    </div>
+</div>
+</footer>
---
+++ block/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <title>Школа</title>
+</head>
+<body>
+    <!-- Шапка -->
+    <header>
+        <div class="header-container">
+            <!-- Логотип -->
+            <div class="logo">
+                <img src="img/gerb_petuhovskogo_rayona.gif" alt="logo">
+            </div>
+            <!-- Название организации -->
+            <div class="header-org">
+                <div class="org-type">Муниципальное бюджетное общеобразовательное учреждение</div>
+                <div class="org-name">Актабанская средняя общеобразовательная школа</div>
+                <div class="org-name-place">с.Актабан Петуховский муниципальный округ Курганская область</div>
+            </div>
+        </div>
+        <!-- Меню -->
+        <div class="menu">
+            <nav role="navigation">
+                <ul id="TopMenu">
+                    <li class="nav"><a class="nav-link" href="index.php">Главная</a></li>
+
+                    <li class="dropdown"><a class="nav-link" href="#">Сведения об образовательной организации</a>
+                        <ul class="dropdown-menu">
+                            <a class="nav-link" href="intelligence.php">Основные сведения</a>
+                            <a href="#">Структура и органы управления образовательной организацией</a>
+                            <a href="#">Документы</a>
+                            <a href="#">Образование</a>
+                            <a href="#">Образовательные стандарты и требования</a>
+                            <a href="compound.php">Руководство. Педагогический (научно-педагогический) состав</a>
+                            <a href="#">Материально-техническое обеспечение и оснащенность образовательного процесса</a>
+                            <a href="#">Платные образовательные услуги</a>
+                            <a href="#">Финансово-хозяйственная деятельность</a>
+                            <a href="#">Вакантные места для приема (перевода) обучающихся</a>
+                            <a href="available.php">Доступная среда</a>
+                            <a href="#">Международное сотрудничество</a>
+                            <a href="#">Организация питания</a>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a class="nav-link" href="about.php">О школе</a>
+                    </li>
+
+                    <li class="dropdown"><a class="nav-link" href="services.php">Услуги и сервисы</a>
+                    </li>
+
+                    <li class="dropdown"><a class="nav-link" href="#">Полезная информация</a>
+                </li>
+
+                    <li class="dropdown"><a class="nav-link" href="сontacts.php">Контакты</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
